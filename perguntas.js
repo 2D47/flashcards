54048cb63ebe8e32113d967236713d784d3b5ba1
2D47 <00001102182412sp@al.educacao.sp.gov.br>
2024-12-02 11:39:31 -0300

    gb

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Charada Interessante',
+    '1. O que é, o que é? Sem sair do seu cantinho, é capaz de viajar ao redor do mundo?',
+    'Resposta: o selo.'
+)
+
+criaCartao(
+    'Charada Inteligente',
+    'O que é, o que é? É alta quando jovem e baixinha quando fica velha. Além disso, é rápida quando é magra e lenta quando é gorda.',
+    'Resposta: a vela.'
+)
+
+criaCartao(
+    'Charada Interessante',
+    'O que é, o que é? Anda sobre quatro membros de manhã, dois à tarde e três à noite.',
+    'Resposta: o ser humano, pois ele engatinha quando é bebê, anda sobre duas pernas quando cresce e usa bengala quando fica velhinho.'
+)
+
+criaCartao(
+    'Piada Engracada',
+    'O que um pintinho falou pro outro?',
+    'Resposta: "piu".'
+)
